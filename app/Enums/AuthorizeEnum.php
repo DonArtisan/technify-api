ad8db88356ef58cce49109eca74e1c9608c4d948
DonArtisan <1db0d24ded>
2022-10-10 21:58:10 -0600

    new enum

---
+++ app/Enums/AuthorizeEnum.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+/**
+ * @method static static DECLINED()
+ * @method static static APPROVED ()
+ */
+final class AuthorizeEnum extends Enum
+{
+    const DECLINED = 0;
+    const APPROVED = 1;
+}
