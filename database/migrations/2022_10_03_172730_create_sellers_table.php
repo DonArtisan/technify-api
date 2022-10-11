c83a1c79fc08e802226b9d9da7e0e7d600c9a1be
DonArtisan <1db0d24ded>
2022-10-11 11:00:28 -0600

    feat: typo errors

--- database/migrations/2022_10_03_172730_create_sellers_table.php
+++ database/migrations/2022_10_03_172730_create_sellers_table.php
@@ -15,12 +15,12 @@ return new class extends Migration
     {
         Schema::create('sellers', function (Blueprint $table) {
             $table->id();
-            $table->string('firstName');
-            $table->string('lastName');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->string('password');
             $table->string('carnet')->unique();
-            $table->dateTime('hired_at');
+            $table->date('hired_at');
             $table->timestamps();
         });
     }
