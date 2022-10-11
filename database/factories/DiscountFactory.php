b76f609f6eee9a855c54bca8c5432174baaee8ab
DonArtisan <1db0d24ded>
2022-10-10 22:03:09 -0600

    factories for maria xd

--- database/factories/DiscountFactory.php
+++ database/factories/DiscountFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Brand>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Discount>
  */
-class BranchFactory extends Factory
+class DiscountFactory extends Factory
 {
     /**
      * Define the model's default state.
