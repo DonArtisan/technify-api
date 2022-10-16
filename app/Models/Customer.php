acf246e07c3089ba6a70cbe2ebffbf33e0175972
DonArtisan <1db0d24ded>
2022-10-15 21:52:32 -0600

    feat: adding new trait

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Silber\Bouncer\Database\HasRolesAndAbilities;
 
 class Customer extends Model
 {
     use HasFactory;
+    use HasRolesAndAbilities;
 }
