06f9f697e06d11b1d3143385bdda9e0b2934be61
Maria Gomez <16159494+mariabug@users.noreply.github.com>
2022-10-23 17:54:31 -0600

    added product seeder

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $categories = Category::pluck('id');
+//        $categories = \App\ProductCategory::pluck('id');
+        $categories->each(function ($category) {
+            Product::factory()->count(10)->create([
+                'category_id' => $category,
+            ]);
+        });
+        //
+
+    }
+}
