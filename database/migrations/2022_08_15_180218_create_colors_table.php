ef324c7d0306675659be02218130a3199896eb88
DonArtisan <1db0d24ded>
2022-11-07 15:56:07 -0600

    feat: search category id

--- database/migrations/2022_08_15_180218_create_colors_table.php
+++ database/migrations/2022_08_15_180218_create_colors_table.php
@@ -17,6 +17,7 @@ return new class () extends Migration {
             $table->string('name');
             $table->timestamps();
         });
+        $this->setInitialValues();
     }
 
     public function setInitialValues(): void
