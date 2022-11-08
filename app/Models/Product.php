1fefeb682abb5bdb8914f56593b9819912356c73
DonArtisan <1db0d24ded>
2022-11-07 22:36:22 -0600

    feat: price table, i think Carlos need to refactor the adding product form

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model as BaseModel;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Product extends BaseModel
@@ -44,5 +45,10 @@ class Product extends BaseModel
     public function color(): HasOne
     {
         return $this->hasOne(Color::class);
+    }
+
+    public function prices(): HasMany
+    {
+        return $this->hasMany(Price::class);
     }
 }
