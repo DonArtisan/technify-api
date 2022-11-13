3e21f67b8645ab8beeffb96d464ecb653377f2be
DonArtisan <1db0d24ded>
2022-11-12 19:54:28 -0600

    feat: relationship in customer model

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Silber\Bouncer\Database\HasRolesAndAbilities;
 
 class Customer extends Model
 {
     use HasFactory;
     use HasRolesAndAbilities;
+
+    public function sales(): MorphMany
+    {
+        return $this->morphMany(ProductSale::class, 'buyerable');
+    }
 }
