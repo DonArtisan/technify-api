a493e978b638cac2f8a3126313ea6e0e008fd9c1
mariabug <mariabug@users.noreply.github.com>
2022-11-13 20:09:15 +0000

    Fix code styling

--- database/factories/ProductSaleDetailFactory.php
+++ database/factories/ProductSaleDetailFactory.php
@@ -24,21 +24,19 @@ class ProductSaleDetailFactory extends Factory
         $sale = ProductSale::factory();
         return [
             'sale_id' => $sale->id,
-            'product_id'=> $product->id,
-            'quantity'=> $this->faker->numberBetween(1,10),
-            'price'=> $price,
+            'product_id' => $product->id,
+            'quantity' => $this->faker->numberBetween(1, 10),
+            'price' => $price,
         ];
-
     }
 
     public function configure()
     {
-        return $this->afterCreating(function (ProductSaleDetail $saleDetail)
-        {
+        return $this->afterCreating(function (ProductSaleDetail $saleDetail) {
             $sale = $saleDetail->productSale()->get()->first();
             $amount = $sale->amount + ($saleDetail->price * $saleDetail->quantity);
             $total = ($sale->tax * $amount) + $amount;
-            $sale->update(['amount' => $amount, 'total'=> $total]);
+            $sale->update(['amount' => $amount, 'total' => $total]);
         });
     }
 }
--- database/factories/ProductSaleFactory.php
+++ database/factories/ProductSaleFactory.php
@@ -19,41 +19,35 @@ class ProductSaleFactory extends Factory
      */
     public function definition()
     {
-
         $buyerable = $this->buyerable();
 
         return [
-            'buyerable_id'=> $buyerable::factory(),
-            'buyerable_type'=> $buyerable,
+            'buyerable_id' => $buyerable::factory(),
+            'buyerable_type' => $buyerable,
             'amount' => 0,
             'tax' => 0.15,
-            'total'=> 0
+            'total' => 0,
         ];
     }
 
     public function buyerable()
     {
-
         return $this->faker->randomElement([
             User::class,
         ]);
-
-
     }
 
     public function configure()
     {
         logger('what');
 
-        return $this->afterCreating(function (ProductSale $sale)
-        {
+        return $this->afterCreating(function (ProductSale $sale) {
             $r = rand(1, 3);
-            for($i = 0; $i < $r; $i++ ){
+            for ($i = 0; $i < $r; $i++) {
                 ProductSaleDetail::factory()->create([
                     'sale_id' => $sale->id,
                 ]);
             }
         });
     }
-
 }
