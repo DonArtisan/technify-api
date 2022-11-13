974da6e67d078a726064d76701f11c7d02235426
DonArtisan <1db0d24ded>
2022-11-12 19:55:28 -0600

    feat: payment with stripe, maybe doesnt work

--- app/GraphQL/ProductSale/Mutations/ProductSaleCreateMutation.php
+++ app/GraphQL/ProductSale/Mutations/ProductSaleCreateMutation.php
@@ -4,6 +4,7 @@ namespace App\GraphQL\ProductSale\Mutations;
 
 use App\GraphQL\Mutations\BaseMutation;
 use App\Models\ProductSale;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\DB;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 
@@ -20,15 +21,22 @@ class ProductSaleCreateMutation extends BaseMutation
             /** @var \App\Models\User $user */
             $user = $context->user();
 
-            $user->sales()->create(
+            $user->createAsStripeCustomer();
+            $user->updateDefaultPaymentMethod($args['input']['paymentMethod']);
+
+            $productSale = $user->sales()->create(
                 Arr::only(
                     $args['input'],['amount', 'tax', 'total'],
                 )
             );
 
-            $ProductSale = ProductSale::class;
+            $productSale->productDetails()->create(
+                Arr::only(
+                    $args['input'],['productId', 'quantity', 'price'],
+                )
+            );
 
-
+            $user->invoiceFor('One Time Fee', $args['input']['amount']);
             DB::commit();
         } catch (Throwable $error) {
             DB::rollBack();
@@ -37,7 +45,7 @@ class ProductSaleCreateMutation extends BaseMutation
         }
 
         return [
-            'productSale' => $ProductSale,
+            'productSale' => $productSale,
             'userErrors' => [],
         ];
     }
