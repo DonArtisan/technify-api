94028b22438a23a985dea5e8e3896b2dc3f6f4b7
DonArtisan <1db0d24ded>
2022-11-13 20:54:33 -0600

    feat: I dont see future in this

---
+++ app/GraphQL/ProductSale/Mutations/ClientSecretMutation.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\GraphQL\ProductSale\Mutations;
+
+use App\GraphQL\Mutations\BaseMutation;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\DB;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+
+class ClientSecretMutation extends BaseMutation
+{
+    /**
+     * @param  null  $_
+     * @param  array{}  $args
+     */
+    public function handle(mixed $root, array $args, GraphQLContext $context): array
+    {
+        DB::beginTransaction();
+        try {
+            /** @var \App\Models\User $user */
+            $user = $context->user();
+
+            $payment = $user->pay(
+                $args['input']['amount']
+            );
+
+            DB::commit();
+        } catch (Throwable $error) {
+            DB::rollBack();
+
+            throw new Error($error);
+        }
+
+        return [
+            'clientSecret' => $payment->client_secret,
+            'userErrors' => [],
+        ];
+    }
+
+    public function rules(): array
+    {
+        return [
+
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+
+        ];
+    }
+
+}
