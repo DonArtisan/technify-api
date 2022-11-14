31e664538cbf9abc9f8519ea77fa18259cfba8b4
CarlosDuarte17 <carlosad154@gmail.com>
2022-11-14 16:49:12 -0600

    fix: sale view name

--- resources/views/livewire/sales.blade.php
+++ resources/views/livewire/sales.blade.php
@@ -2,7 +2,7 @@
     <div class="p-4 bg-white block sm:flex items-center justify-between lg:mt-1.5 lg:pt-12">
         <div class="mb-1 w-full">
             <div class="mb-4">
-                <h1 class="text-xl sm:text-2xl font-semibold text-gray-900">Orders</h1>
+                <h1 class="text-xl sm:text-2xl font-semibold text-gray-900">Ventas</h1>
             </div>
             <div class="sm:flex">
                 <div class="hidden sm:flex items-center sm:divide-x sm:divide-gray-100 mb-3 sm:mb-0">
@@ -18,7 +18,7 @@
                 <div class="flex items-center space-x-2 sm:space-x-3 ml-auto">
                     <button wire:click="showAddModal" type="button" data-modal-toggle="add-user-modal" class="w-1/2 text-white bg-cyan-600 hover:bg-cyan-700 focus:ring-4 focus:ring-cyan-200 font-medium inline-flex items-center justify-center rounded-lg text-sm px-3 py-2 text-center sm:w-auto">
                         <svg class="-ml-1 mr-2 h-6 w-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
-                        Agregar order
+                        Realizar venta
                     </button>
                     <a href="#" class="w-1/2 text-gray-900 bg-white border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-cyan-200 font-medium inline-flex items-center justify-center rounded-lg text-sm px-3 py-2 text-center sm:w-auto">
                         <svg class="-ml-1 mr-2 h-6 w-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm5 6a1 1 0 10-2 0v3.586l-1.293-1.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V8z" clip-rule="evenodd"></path></svg>
