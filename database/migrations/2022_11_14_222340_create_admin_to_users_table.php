4be3a1da78f5056601f9e8ba0d6b0c376f064690
DonArtisan <DonArtisan@users.noreply.github.com>
2022-12-02 17:54:45 +0000

    Fix code styling

--- database/migrations/2022_11_14_222340_create_admin_to_users_table.php
+++ database/migrations/2022_11_14_222340_create_admin_to_users_table.php
@@ -3,17 +3,16 @@
 use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     public function up(): void
     {
         // Create Admin
-         User::factory()->createOne([
+        User::factory()->createOne([
             'first_name' => 'Technify',
             'email' => 'admin@technify.com',
             'password' => bcrypt('Technify1234'),
             'is_admin' => true,
-            'last_name' => 'Admin'
+            'last_name' => 'Admin',
         ]);
     }
 };
